1af281682cebf90d6f66f4772cdedb40bb119c7b
anakayama010 <121213017+anakayama010@users.noreply.github.com>
2023-03-07 16:38:13 +0900

    ゲーム進行クラスを作成

---
+++ Main.java
@@ -0,0 +1,11 @@
+/**
+ * ゲーム進行クラス
+ */
+public class Main {
+	public static void main(String[] args){
+		final String[] STATUS = {"●", "○", "◌"}; //[●, ○, ◌]
+
+		//ゲームを初期化する
+		GameBoard banmen = new GameBoard();
+	}
+}
