13a80dff227218dc95c8a1584bbb2f71cb5973af
anakayama010 <121213017+anakayama010@users.noreply.github.com>
2023-03-08 15:21:43 +0900

    Astahの図を元にメソッドを追加しました。

--- JavaConsole/Board.java
+++ JavaConsole/Board.java
@@ -7,10 +7,10 @@ package javaconsole;
  */
 public class Board {
 	/** 手番 */
-	int turn = 1;
+	private int turn = 1;
 
 	/** 盤面の状態(空白=0, 黒=1, 白=2) */
-	int[][] square = new int[4][4];
+	private int[][] square = new int[4][4];
 
 	//コンストラクタ（初期化）
 	public Board(){
@@ -43,5 +43,51 @@ public class Board {
 			}
 		}
 		return count;
+	}
+
+	/** 白の数を取得するメソッド */	
+	public int getCountWhite(){
+		int count = 0;
+		for(int[] a : square){
+			for(int b : a){
+				if(b == 2){
+					count++;
+				}
+			}
+		}
+		return count;
+	}
+
+	/** 手番を変えるメソッド */
+	public void changeTurn(){
+		if(turn == 1)
+		turn = 2;
+		else
+		turn = 1;
+	}
+
+	/** 手番を取得するメソッド */
+	public int getTurn(){
+		return turn;
+	}
+
+	/** 盤面にまだ置かれていないマスがあるか確認するメソッド */
+	public boolean isThereNull(){
+		boolean isNull = false;
+		for(int[] a : square){
+			System.out.print("[ ");//確認用
+			for(int b : a){
+				System.out.print(b + " ");//確認用
+				if(b == 0){
+					isNull = true;
+					break;
+				}
+			}
+			if(isNull)
+			break;
+			System.out.println("]");//確認用
+		}
+		System.out.println("");//確認用
+		return isNull;
 	}
 }
