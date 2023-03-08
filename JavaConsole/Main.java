f9867077a70afc5c9399fbbbf7bbde8d02780228
anakayama010 <121213017+anakayama010@users.noreply.github.com>
2023-03-08 15:26:04 +0900

    他クラスに追加したメソッドの動作確認するためのメソッドを追加しました。

--- JavaConsole/Main.java
+++ JavaConsole/Main.java
@@ -13,9 +13,36 @@ package javaconsole;
 public class Main {
 	public static void main(String[] args){
 
-		//ゲームを初期化して生成
+		/** ゲームを初期化して生成 */
 		Board board = new Board();
 
-		System.out.println(board.getCountBlack());
+		/** 動作確認の実行 */
+		methodCheck(board);
+
 	}
+	/** 各種メソッドの動作確認を行うメソッド */
+	public static void methodCheck(Board board){
+		//board.getBoard()
+		int[][] square = board.getBoard();
+		for(int[] a : square){
+			System.out.print("[ ");
+			for(int b : a){
+				System.out.print(b + " ");
+			}
+			System.out.println("]");
+		}
+		//board.getCountBlack()
+		System.out.println("Black : " + board.getCountBlack());
+		//board.getCountWhite()
+		System.out.println("White : " + board.getCountWhite());
+		//board.getTurn()
+		System.out.println("Turn : " + board.getTurn());
+		//board.changeTurn()
+		for(int i = 1; i <= 5; i++){
+			board.changeTurn();
+			System.out.println("AfterChange " + i + " Turn : " + board.getTurn());
+		}
+		//isThereNull()
+		System.out.println("isNull : " + board.isThereNull());
+	} 
 }
