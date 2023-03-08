e379a00409380932c7dfce9e08372d1f24cc3a1b
anakayama010 <121213017+anakayama010@users.noreply.github.com>
2023-03-08 23:55:20 +0900

    横判定メソッドを編集中。

--- JavaConsole/Judge.java
+++ JavaConsole/Judge.java
@@ -26,6 +26,27 @@ public class Judge {
 	/** 横に置けるかどうか判定 */
 	private static boolean placeableW(int i, int j){
 		System.out.println(square[i][j] + "(" + i + "," + j + ")について横判定");
+		try{
+			if(square[i][++j] == turn){//右隣が同じ色 → false
+				System.out.print("[" + i + "][" + ++j + "]" + " == " + turn)
+				return false;}
+			while(square[i][j+1] != 0){//右隣が空白でないとき
+				if(square[i][j+1] == turn){//対となる同色があった
+					System.out.print("[" + i + "][" + j+1 + "]" + " == " + turn)
+					return true;
+				}else if(square[i][j+1] == 0){//対がないまま空白に辿り着いた
+					System.out.print("[" + i + "][" + j+1 + "]" + " == " + 0)
+					return false;
+				}else{//相手の色が続いている
+					System.out.print("[" + i + "][" + j+1 + "]" + " != " + turn + "(else)")
+					j++;
+					continue;
+				}
+			}
+		}catch(IndexOutOfBoundsException){
+			System.out.print("catch ")
+			return false;
+		}
 
 		if(square[i][j+1] == turn)//右隣が同じ色 → false
 		return false;
