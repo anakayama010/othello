760d696c926f3cae94e84ce6f867e2033a8e6be0
anakayama010 <121213017+anakayama010@users.noreply.github.com>
2023-03-10 10:40:56 +0900

    変更

--- JavaConsole/Judge.java
+++ JavaConsole/Judge.java
@@ -29,56 +29,27 @@ public class Judge {
 		System.out.println(square[i][j] + "(" + i + "," + j + ")について横判定");
 		try{
 			if(square[i][++j] == turn){//右隣が同じ色 → false
-				System.out.print("[" + i + "][" + ++j + "]" + " == " + turn + " ")
-				return false;}
+				System.out.print("[" + i + "][" + ++j + "]" + " == " + turn + " ");
+				return false;
+			}
 			while(square[i][j+1] != 0){//右隣が空白でないとき
 				if(square[i][j+1] == turn){//対となる同色があった
-					System.out.print("[" + i + "][" + j+1 + "]" + " == " + turn + " ")
+					System.out.print("[" + i + "][" + j+1 + "]" + " == " + turn + " ");
 					return true;
 				}else if(square[i][j+1] == 0){//対がないまま空白に辿り着いた
-					System.out.print("[" + i + "][" + j+1 + "]" + " == " + 0 + " ")
+					System.out.print("[" + i + "][" + j+1 + "]" + " == " + 0 + " ");
 					return false;
 				}else{//相手の色が続いている
-					System.out.print("[" + i + "][" + j+1 + "]" + " != " + turn + "(else) ")
+					System.out.print("[" + i + "][" + j+1 + "]" + " != " + turn + "(else) ");
 					j++;
 					continue;
 				}
 			}
-		}catch(IndexOutOfBoundsException){
-			System.out.print("catch ")
+		}catch(IndexOutOfBoundsException e){
+			System.out.print("catch ");
 			return false;
 		}
-
-		if(square[i][j+1] == turn)//右隣が同じ色 → false
-		return false;
-
-		while(square[i][j+1] != 0){//右隣が空白でないとき
-			if(square[i][j+1] == turn){//対となる同色があった
-				return true;
-			}else if(square[i][j+1] == 0){//対がないまま空白に辿り着いた
-				return false;
-			}else{//相手の色が続いている
-				continue;
-			}
-		}
-
-		switch(square[i][j+1]){
-			case 0:
-				break;
-			case 1:
-				if(turn == 1){
-					break;
-				}else{
-					return true;
-				}
-			case 2:
-				if(turn == 1){
-					return true;
-				}else{
-					break;
-				}
-
-		}
+		
 	}
 	
 }
